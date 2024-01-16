9d82c842f0b74684df745fb4db248d31b06db91a
Alexander Lutsenko <alexxx.lutsenko@gmail.com>
2024-01-16 13:44:47 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+/* Для React
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+*/
+
+import './index.html';
+import './fonts/fonts.scss';
+import './index.scss';
+import './js/hello.js';
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? 'development',
+        entry: "./src/index.js",
+        output: {
+            filename: "main[contenthash].js",
+            path: path.resolve(__dirname, 'build'),
+            clean: true
+        },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],  
+            },
+            {
+               test: /\.(png|jpg|jpeg|gif)$/i,
+               type: 'asset/resource',
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                filename: path.join('icons', '[name].[contenthash][ext]'),
+                },
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'main.html',
+            inject: 'body'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    optimization: {
+        minimizer: [
+            new ImageMinimizerPlugin({
+             minimizer: {
+                implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                         plugins: [
+                            ['gifsicle', { interlaced: true }],
+                            ['jpegtran', { progressive: true }],
+                            ['optipng', { optimizationLevel: 5 }],
+                            ['svgo', { name: 'preset-default' }],
+                        ],
+                    },
+                },
+            }),
+        ],
+     },
+    devServer: {
+        port: 9000,
+        compress: true,
+        hot: true,
+        open: true,
+        static: {
+            directory: path.resolve(__dirname, 'build'),
+        },
+    }
+}
+}
