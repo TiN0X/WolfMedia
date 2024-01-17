7762991755b1821e74b5e68ad21e6283558e2ceb
TiNoX <109901925+TiN0X@users.noreply.github.com>
2024-01-17 15:58:15 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -38,21 +38,21 @@ module.exports = (env) => {
                test: /\.(png|jpg|jpeg|gif)$/i,
                type: 'asset/resource',
                generator: {
-                    filename: path.resolve('images', '[name].[contenthash][ext]'),
+                    filename: path.join('images', '[name].[contenthash][ext]'),
                }
             },
             {
                 test: /\.svg$/,
                 type: 'asset/resource',
                 generator: {
-                    filename: path.resolve('icons', '[name].[contenthash][ext]'),
+                    filename: path.join('icons', '[name].[contenthash][ext]'),
                 },
             },
             {
                 test: /\.(woff2?|eot|ttf|otf)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: path.resolve('fonts', '[name].[contenthash][ext]'),
+                    filename: path.join('fonts', '[name].[contenthash][ext]'),
                 }
             }
         ],
