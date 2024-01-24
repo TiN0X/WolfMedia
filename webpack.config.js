0dff426c73e703cc0eb80e2b04e15ac144140028
TiNoX <109901925+TiN0X@users.noreply.github.com>
2024-01-24 16:07:28 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -38,21 +38,21 @@ module.exports = (env) => {
                test: /\.(png|jpg|jpeg|gif)$/i,
                type: 'asset/resource',
                generator: {
-                    filename: path.join('images', '[name].[contenthash][ext]'),
+                    filename: path.join('assets', '[name].[contenthash][ext]'),
                }
             },
             {
                 test: /\.svg$/,
                 type: 'asset/resource',
                 generator: {
-                    filename: path.join('icons', '[name].[contenthash][ext]'),
+                    filename: path.join('assets', '[name].[contenthash][ext]'),
                 },
             },
             {
                 test: /\.(woff2?|eot|ttf|otf)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: path.join('fonts', '[name].[contenthash][ext]'),
+                    filename: path.join('assets', '[name].[contenthash][ext]'),
                 }
             }
         ],
@@ -62,9 +62,6 @@ module.exports = (env) => {
             template: path.resolve(__dirname, 'src', 'index.html'),
             filename: 'main.html',
             inject: 'body'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
         }),
     ],
     optimization: {
